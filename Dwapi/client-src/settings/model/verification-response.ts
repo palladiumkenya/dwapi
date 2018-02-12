531e417ee42fcb18fc6a41009b866e39a697a1c8
Koske Kimutai <koskedk@live.com>
2018-02-12 15:18:28 +0300

    registry validaton clean up

---
+++ Dwapi/client-src/settings/model/verification-response.ts
@@ -0,0 +1,12 @@
+export interface Extract {
+    id?: string;
+    name?: string;
+    display?: string;
+    extractSql?: string;
+    destination?: string;
+    rank?: string;
+    isPriority?: string;
+    docketId?: string;
+    emrSystemId?: string;
+    emr?: string;
+}
