0c0d9ea8f9ab2af727d4c89be10f1d735fa3a8a2
Brian Mwasi <mwasi.brian@gmail.com>
2018-05-21 13:06:58 +0300

    Extract details UI partial

--- Dwapi/client-src/dockets/ndwh-docket/ndwh-extract-details/ndwh-extract-details.component.ts
+++ Dwapi/client-src/dockets/ndwh-docket/ndwh-extract-details/ndwh-extract-details.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    OnChanges,
+    Input,
+} from '@angular/core';
+import {Extract} from '../../../settings/model/extract';
+import { NdwhConsoleComponent } from '../ndwh-console/ndwh-console.component';
+import { EmrSystem } from '../../../settings/model/emr-system';
+import { EmrConfigService } from '../../../settings/services/emr-config.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Message } from 'primeng/api';
+
 
 @Component({
-  selector: 'liveapp-ndwh-extract-details',
-  templateUrl: './ndwh-extract-details.component.html',
-  styleUrls: ['./ndwh-extract-details.component.scss']
+    selector: 'liveapp-ndwh-extract-details',
+    templateUrl: './ndwh-extract-details.component.html',
+    styleUrls: ['./ndwh-extract-details.component.scss']
 })
 export class NdwhExtractDetailsComponent implements OnInit {
+    private _emrConfigService: EmrConfigService;
+    public getEmr$: Subscription;
+    public extracts: Extract[] = [];
+    public errorMessage: Message[];
+    public otherMessage: Message[];
+    public constructor(emrConfigService: EmrConfigService) {
+        this._emrConfigService = emrConfigService;
+     }
+    public ngOnInit() {
+        this.getExtract();
+    }
 
+    public getExtract(): Extract[] {
+        this.getEmr$ = this._emrConfigService.getDefault()
+          .subscribe(
+              p => {
+                  this.extracts = p.extracts.filter(x => x.docketId === 'NDWH');
+                  return this.extracts;
+              },
+              e => {
+                  this.errorMessage = [];
+                  this.errorMessage.push({severity: 'error', summary: 'Error Loading data', detail: <any>e});
+              },
+              () => {
 
-  constructor() { }
-
-  ngOnInit() {  }
-
+              }
+          );
+          return this.extracts;
+    }
 }
