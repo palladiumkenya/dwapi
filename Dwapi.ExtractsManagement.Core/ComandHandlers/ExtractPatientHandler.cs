660ba226739fb302e02de590bea791d6bd07a3ed
Brian Mwasi <mwasi.brian@gmail.com>
2018-05-23 12:24:43 +0300

    patient extract command and reader

---
+++ Dwapi.ExtractsManagement.Core/ComandHandlers/ExtractPatientHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dwapi.ExtractsManagement.Core.Commands;
+using MediatR;
+
+namespace Dwapi.ExtractsManagement.Core.ComandHandlers
+{
+    public class ExtractPatientHandler :IRequestHandler<ExtractPatient>
+    {
+        public Task Handle(ExtractPatient request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Dwapi.ExtractsManagement.Core/Interfaces/Reader/Dwh/IPatientSourceReader.cs
@@ -0,0 +1,9 @@
+﻿using Dwapi.ExtractsManagement.Core.Model.Source.Dwh;
+using Dwapi.SharedKernel.Interfaces;
+
+namespace Dwapi.ExtractsManagement.Core.Interfaces.Reader.Dwh
+{
+    public interface IPatientSourceReader : ISourceReader<TempPatientExtract>
+    {
+    }
+}
