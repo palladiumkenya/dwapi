a10538b71f7577a6c0496d9568976c5dc6e4c983
Brian Mwasi <mwasi.brian@gmail.com>
2018-05-24 12:38:19 +0300

    validator repository

---
+++ Dwapi.ExtractsManagement.Core/Interfaces/Repository/Dwh/IValidatorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Dwapi.ExtractsManagement.Core.Model;
+using Dwapi.SharedKernel.Interfaces;
+
+namespace Dwapi.ExtractsManagement.Core.Interfaces.Repository.Dwh
+{
+    public interface IValidatorRepository: IRepository<Validator,Guid>
+    {
+        
+    }
+}
---
+++ Dwapi.ExtractsManagement.Infrastructure/Repository/Dwh/ValidatorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Dwapi.ExtractsManagement.Core.Interfaces.Repository.Dwh;
+using Dwapi.ExtractsManagement.Core.Model;
+using Dwapi.SharedKernel.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dwapi.ExtractsManagement.Infrastructure.Repository.Dwh
+{
+    public class ValidatorRepository:BaseRepository<Validator, Guid> , IValidatorRepository
+    {
+        public ValidatorRepository(ExtractsContext context) : base(context)
+        {
+        }
+    }
+}
