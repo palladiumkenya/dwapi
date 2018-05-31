e2f79e4ed3b3c4870f3edefa45d18fbed4b107fc
Koske Kimutai <koskedk@live.com>
2018-05-31 09:26:42 +0300

    load from emr fix

--- Dwapi/Startup.cs
+++ Dwapi/Startup.cs
@@ -129,15 +129,13 @@ namespace Dwapi
             services.AddScoped<IPatientValidator, PatientValidator>();
             services.AddScoped<IPatientLoader, PatientLoader>();
             services.AddScoped<IClearExtracts, ClearExtracts>();
-
-            
-
+            ServiceProvider = services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
-            ServiceProvider = serviceProvider;
+           
 
             if (env.IsDevelopment())
             {
