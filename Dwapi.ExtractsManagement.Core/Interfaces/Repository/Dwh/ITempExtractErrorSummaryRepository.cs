a915bfdb624766b734b275cefb878e480858fcf1
Brian Mwasi <mwasi.brian@gmail.com>
2018-06-13 11:30:04 +0300

    removed paged list

--- Dwapi.ExtractsManagement.Core/Interfaces/Repository/Dwh/ITempExtractErrorSummaryRepository.cs
+++ Dwapi.ExtractsManagement.Core/Interfaces/Repository/Dwh/ITempExtractErrorSummaryRepository.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
-using PagedList;
 
 namespace Dwapi.ExtractsManagement.Core.Interfaces.Repository.Dwh
 {
     public interface ITempExtractErrorSummaryRepository<T>
     {
         IEnumerable<T> GetAll();
-        IPagedList<T> GetAll(int? page, int? pageSize, string search = "");
         IEnumerable<T> GetByExtract(string extract);
     }
 }
--- Dwapi.ExtractsManagement.Infrastructure/Repository/Dwh/TempExtractErrorSummaryRepository.cs
+++ Dwapi.ExtractsManagement.Infrastructure/Repository/Dwh/TempExtractErrorSummaryRepository.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Dwapi.ExtractsManagement.Core.Interfaces.Repository.Dwh;
 using Dwapi.ExtractsManagement.Core.Model.Source.Dwh;
 using Microsoft.EntityFrameworkCore;
-using PagedList;
 
 namespace Dwapi.ExtractsManagement.Infrastructure.Repository.Dwh
 {
@@ -21,29 +20,6 @@ namespace Dwapi.ExtractsManagement.Infrastructure.Repository.Dwh
         public IEnumerable<T> GetAll()
         {
             return DbSet.AsNoTracking();
-        }
-
-        public IPagedList<T> GetAll(int? page, int? pageSize, string search = "")
-        {
-            page = page == 0 ? 1 : page;
-            pageSize = pageSize == 0 ? 100 : pageSize;
-            pageSize = pageSize == -1 ? GetAll().Count() : pageSize;
-
-            var tempExtracts = GetAll();
-
-            if (!string.IsNullOrWhiteSpace(search))
-            {
-                tempExtracts = tempExtracts.Where(n =>
-                    n.PatientID.ToLower().Contains(search.ToLower())
-                );
-            }
-
-
-            var pagedlist = tempExtracts
-                .OrderBy(x => x.PatientID).ThenBy(x=>x.Summary)
-                .ToPagedList(page.Value, pageSize.Value);
-
-            return pagedlist;
         }
 
         public IEnumerable<T> GetByExtract(string extract)
