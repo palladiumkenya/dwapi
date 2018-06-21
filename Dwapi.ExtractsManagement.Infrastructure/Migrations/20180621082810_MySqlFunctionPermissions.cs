7d9fa59b27fb17725ccd39512e2a3d9391840e9a
Brian Mwasi <mwasi.brian@gmail.com>
2018-06-21 11:57:57 +0300

    added mysql grant access migrations

---
+++ Dwapi.ExtractsManagement.Infrastructure/Migrations/20180621082810_MySqlFunctionPermissions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
+{
+    public partial class MySqlFunctionPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.ToLower().Contains("MySql".ToLower()))
+            {
+                migrationBuilder.Sql(@"
+                GRANT EXECUTE ON FUNCTION fn_calculateJaro TO '*'@'%';
+                GRANT EXECUTE ON FUNCTION fn_calculateJaroWinkler TO '*'@'%';
+                GRANT EXECUTE ON FUNCTION fn_calculateMatchWindow TO '*'@'%';
+                GRANT EXECUTE ON FUNCTION fn_calculatePrefixLength TO '*'@'%';
+                GRANT EXECUTE ON FUNCTION fn_calculateTranspositions TO '*'@'%';
+                GRANT EXECUTE ON FUNCTION fn_GetCommonCharacters TO '*'@'%';");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
