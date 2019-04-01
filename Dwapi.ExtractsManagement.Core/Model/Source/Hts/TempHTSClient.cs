a69174a7374569e6b52af9dd6c326a64e95ab3ea
MSDN Admin <FGKenyaHMIS@hotmail.com>
2019-04-01 15:56:03 +0300

    TempHTSClient definition

--- Dwapi.ExtractsManagement.Core/Model/Source/Hts/TempHTSClient.cs
+++ Dwapi.ExtractsManagement.Core/Model/Source/Hts/TempHTSClient.cs
@@ -9,7 +9,29 @@ namespace Dwapi.ExtractsManagement.Core.Model.Source.Hts
 {
     public class TempHTSClient : TempHTSExtract
     {
-     
-        
+
+        public DateTime? VisitDate { get; set; }
+        public DateTime? dob { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string KeyPop { get; set; }
+        public string TestedBefore { get; set; }
+        public int MonthsLastTested { get; set; }
+        public string ClientTestedAs { get; set; }
+        public string StrategyHTS { get; set; }
+        public string TestKitName1 { get; set; }
+        public string TestKitLotNumber1 { get; set; }
+        public DateTime? TestKitExpiryDate1 { get; set; }
+        public string TestResultsHTS1 { get; set; }
+        public string TestKitName2 { get; set; }
+        public string TestKitLotNumber2 { get; set; }
+        public string TestKitExpiryDate2 { get; set; }
+        public string TestResultsHTS2 { get; set; }
+        public string finalResultHTS { get; set; }
+        public string FinalResultsGiven { get; set; }
+        public string TBScreeningHTS { get; set; }
+        public string clientSelfTested { get; set; }
+        public DateTime DateImported { get; set; }
+
     }
 }
