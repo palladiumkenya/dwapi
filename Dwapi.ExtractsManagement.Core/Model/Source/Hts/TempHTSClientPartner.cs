aff19a5d4059e48726bbb8ac387d1f7b941fcfbe
MSDN Admin <FGKenyaHMIS@hotmail.com>
2019-04-01 15:21:09 +0300

    HTS Model Classes

---
+++ Dwapi.ExtractsManagement.Core/Model/Source/Hts/TempHTSClientPartner.cs
@@ -0,0 +1,13 @@
+﻿using Dwapi.ExtractsManagement.Core.Model.Source.Dwh;
+using Dwapi.SharedKernel.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dwapi.ExtractsManagement.Core.Model.Source.Hts
+{
+    public class TempHTSClientPartner : TempHTSExtract
+    {
+        
+    }
+}
---
+++ Dwapi.ExtractsManagement.Core/Model/Source/Hts/TempHTSExtract.cs
@@ -0,0 +1,23 @@
+﻿using Dwapi.SharedKernel.Model;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dwapi.ExtractsManagement.Core.Model.Source.Dwh
+{
+    public abstract class TempHTSExtract : Entity<Guid>
+    {
+        public virtual int? PatientPK { get; set; }
+        public virtual string PatientID { get; set; }
+         public virtual int? SiteCode { get; set; }
+        public virtual DateTime DateExtracted { get; set; } = DateTime.Now;
+        public virtual string Emr { get; set; }
+        public virtual string Project { get; set; }
+        public virtual bool CheckError { get; set; }
+        [NotMapped]
+        public bool HasError { get; set; }
+        public override string ToString()
+        {
+            return $"{SiteCode}-{PatientID}";
+        }
+    }
+}
