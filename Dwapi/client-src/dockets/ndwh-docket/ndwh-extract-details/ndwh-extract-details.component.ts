c463564866de48931770929c6c5f35e3d70b210a
Koske Kimutai <koskedk@live.com>
2019-07-11 15:05:57 +0300

    validations reveiw

--- Dwapi/client-src/dockets/ndwh-docket/ndwh-extract-details/ndwh-extract-details.component.ts
+++ Dwapi/client-src/dockets/ndwh-docket/ndwh-extract-details/ndwh-extract-details.component.ts
@@ -3,17 +3,17 @@ import {
     OnInit,
     OnChanges,
     Input,
-    ViewChild,
+    ViewChild, SimpleChanges,
 } from '@angular/core';
 import {Extract} from '../../../settings/model/extract';
-import { NdwhConsoleComponent } from '../ndwh-console/ndwh-console.component';
-import { EmrSystem } from '../../../settings/model/emr-system';
-import { EmrConfigService } from '../../../settings/services/emr-config.service';
-import { Subscription } from 'rxjs/Subscription';
-import { Message, } from 'primeng/api';
+import {NdwhConsoleComponent} from '../ndwh-console/ndwh-console.component';
+import {EmrSystem} from '../../../settings/model/emr-system';
+import {EmrConfigService} from '../../../settings/services/emr-config.service';
+import {Subscription} from 'rxjs/Subscription';
+import {Message,} from 'primeng/api';
 import {TabView, TabPanel} from 'primeng/primeng';
-import { PatientExtract } from '../../models/patient-extract';
-import { NdwhPatientsExtractService } from '../../services/ndwh-patients-extract.service';
+import {PatientExtract} from '../../models/patient-extract';
+import {NdwhPatientsExtractService} from '../../services/ndwh-patients-extract.service';
 
 
 @Component({
@@ -21,7 +21,7 @@ import { NdwhPatientsExtractService } from '../../services/ndwh-patients-extract
     templateUrl: './ndwh-extract-details.component.html',
     styleUrls: ['./ndwh-extract-details.component.scss']
 })
-export class NdwhExtractDetailsComponent implements OnInit {
+export class NdwhExtractDetailsComponent implements OnInit, OnChanges {
     @Input() emr: EmrSystem;
     public getEmr$: Subscription;
     public extracts: Extract[] = [];
@@ -35,6 +35,9 @@ export class NdwhExtractDetailsComponent implements OnInit {
     }
 
     public ngOnInit() {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
         if (this.emr) {
             this.extracts = this.emr.extracts.filter(x => x.docketId === 'NDWH');
         }
