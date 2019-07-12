53db0a34f4ab749d2b1eefbc3ddd7b0c8effc61f
Koske Kimutai <koskedk@live.com>
2019-07-12 15:50:40 +0300

    mysql hts migration fix

--- Dwapi.ExtractsManagement.Infrastructure/Migrations/20190712055507_drophtskey.cs
+++ Dwapi.ExtractsManagement.Infrastructure/Migrations/20190712055507_drophtskey.cs
@@ -6,35 +6,54 @@ namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropUniqueConstraint(
-                name: "AK_HtsClientExtracts_Id",
-                table: "HtsClientExtracts");
+            if (migrationBuilder.ActiveProvider.ToLower().Contains("MySql".ToLower()))
+            {
+                migrationBuilder.Sql(@"DROP INDEX AK_HtsClientExtracts_Id ON HtsClientExtracts");
+                migrationBuilder.Sql(@"ALTER TABLE HtsClientExtracts DROP PRIMARY KEY");
+                migrationBuilder.Sql(@"ALTER TABLE HtsClientExtracts ADD PRIMARY KEY (Id)");
+            }
+            else
+            {
+                migrationBuilder.DropUniqueConstraint(
+                    name: "AK_HtsClientExtracts_Id",
+                    table: "HtsClientExtracts");
 
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_HtsClientExtracts",
-                table: "HtsClientExtracts");
+                migrationBuilder.DropPrimaryKey(
+                    name: "PK_HtsClientExtracts",
+                    table: "HtsClientExtracts");
 
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_HtsClientExtracts",
-                table: "HtsClientExtracts",
-                column: "Id");
+                migrationBuilder.AddPrimaryKey(
+                    name: "PK_HtsClientExtracts",
+                    table: "HtsClientExtracts",
+                    column: "Id");
+            }
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_HtsClientExtracts",
-                table: "HtsClientExtracts");
 
-            migrationBuilder.AddUniqueConstraint(
-                name: "AK_HtsClientExtracts_Id",
-                table: "HtsClientExtracts",
-                column: "Id");
+            if (migrationBuilder.ActiveProvider.ToLower().Contains("MySql".ToLower()))
+            {
+                migrationBuilder.Sql(@"ALTER TABLE HtsClientExtracts DROP PRIMARY KEY");
+                migrationBuilder.Sql(@"ALTER TABLE HtsClientExtracts ADD CONSTRAINT AK_HtsClientExtracts_Id UNIQUE (Id)");
+                migrationBuilder.Sql(@"ALTER TABLE HtsClientExtracts ADD PRIMARY KEY (SiteCode,PatientPk,EncounterId)");
+            }
+            else
+            {
+                migrationBuilder.DropPrimaryKey(
+                    name: "PK_HtsClientExtracts",
+                    table: "HtsClientExtracts");
 
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_HtsClientExtracts",
-                table: "HtsClientExtracts",
-                columns: new[] { "SiteCode", "PatientPk", "EncounterId" });
+                migrationBuilder.AddUniqueConstraint(
+                    name: "AK_HtsClientExtracts_Id",
+                    table: "HtsClientExtracts",
+                    column: "Id");
+
+                migrationBuilder.AddPrimaryKey(
+                    name: "PK_HtsClientExtracts",
+                    table: "HtsClientExtracts",
+                    columns: new[] {"SiteCode", "PatientPk", "EncounterId"});
+            }
         }
     }
 }
