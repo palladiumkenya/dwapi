a3e6f15668af73d5944493624561def54a88bde3
Koske Kimutai <koskedk@live.com>
2019-12-06 13:32:19 +0300

    added dashboard controller

--- Dwapi.SettingsManagement.Core/DTOs/AppMetricDto.cs
+++ Dwapi.SettingsManagement.Core/DTOs/AppMetricDto.cs
@@ -4,9 +4,17 @@ namespace Dwapi.SettingsManagement.Core.DTOs
 {
     public class AppMetricDto
     {
-        public string Name { get; set; }
-        public string Action { get; set; }
-        public DateTime LastAction { get; set; }
-        public int Rank { get; set; }
+        public string Name { get;  }
+        public string Action { get;  }
+        public DateTime LastAction { get;  }
+        public int Rank { get;  }
+
+        public AppMetricDto(string name, string action, DateTime lastAction, int rank)
+        {
+            Name = name;
+            Action = action;
+            LastAction = lastAction;
+            Rank = rank;
+        }
     }
 }
--- Dwapi.SettingsManagement.Infrastructure/Repository/AppMetricRepository.cs
+++ Dwapi.SettingsManagement.Infrastructure/Repository/AppMetricRepository.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Dwapi.SettingsManagement.Infrastructure.Repository
 {
-    public class AppMetricRepository:BaseRepository<AppMetric,Guid>, IAppMetricRepository
+    public class AppMetricRepository : BaseRepository<AppMetric, Guid>, IAppMetricRepository
     {
         public AppMetricRepository(SettingsContext context) : base(context)
         {
@@ -18,35 +18,82 @@ namespace Dwapi.SettingsManagement.Infrastructure.Repository
         {
             var list = new List<AppMetric>();
 
+
             //CareTreatment
 
             var ctLoaded = DbSet.AsNoTracking().Where(x => x.Name == "CareTreatment" && x.LogValue.Contains("NoLoaded"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(ctLoaded);
+            if (null != ctLoaded)
+                list.Add(ctLoaded);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatCt("NoLoaded");
+                list.Add(ap);
+            }
 
             var ctSent = DbSet.AsNoTracking().Where(x => x.Name == "CareTreatment" && x.LogValue.Contains("NoSent"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(ctSent);
+            if (null != ctSent)
+                list.Add(ctSent);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatCt("NoSent");
+                list.Add(ap);
+            }
 
             //HivTestingService
 
-            var htsLoaded = DbSet.AsNoTracking().Where(x => x.Name == "HivTestingService" && x.LogValue.Contains("NoLoaded"))
+            var htsLoaded = DbSet.AsNoTracking()
+                .Where(x => x.Name == "HivTestingService" && x.LogValue.Contains("NoLoaded"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(htsLoaded);
+            if (null != htsLoaded)
+                list.Add(htsLoaded);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatHts("NoLoaded");
+                list.Add(ap);
+            }
 
-            var htsSent = DbSet.AsNoTracking().Where(x => x.Name == "HivTestingService" && x.LogValue.Contains("NoSent"))
+            var htsSent = DbSet.AsNoTracking()
+                .Where(x => x.Name == "HivTestingService" && x.LogValue.Contains("NoSent"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(htsSent);
+            if (null != htsSent)
+                list.Add(htsSent);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatHts("NoSent");
+                list.Add(ap);
+            }
 
             //MasterPatientIndex
 
-            var mpiLoaded = DbSet.AsNoTracking().Where(x => x.Name == "MasterPatientIndex" && x.LogValue.Contains("NoLoaded"))
+            var mpiLoaded = DbSet.AsNoTracking()
+                .Where(x => x.Name == "MasterPatientIndex" && x.LogValue.Contains("NoLoaded"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(mpiLoaded);
+            if (null != mpiLoaded)
+                list.Add(mpiLoaded);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatMpi("NoLoaded");
+                list.Add(ap);
+            }
 
-            var mpiSent = DbSet.AsNoTracking().Where(x => x.Name == "MasterPatientIndex" && x.LogValue.Contains("NoSent"))
+            var mpiSent = DbSet.AsNoTracking()
+                .Where(x => x.Name == "MasterPatientIndex" && x.LogValue.Contains("NoSent"))
                 .OrderByDescending(x => x.LogDate).FirstOrDefault();
-            list.Add(mpiSent);
+            if (null != mpiSent)
+                list.Add(mpiSent);
+            else
+            {
+                var ap = new AppMetric();
+                ap.CreatMpi("NoSent");
+                list.Add(ap);
+            }
 
 
 
