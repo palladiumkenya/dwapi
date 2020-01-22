7c4b56eaa42495ace407116a69df49c0b98b5e14
NgibuiniGikandi <ngibuinigikandi@gmail.com>
2020-01-22 07:14:23 +0300

    Added Faces queries in extract

--- Dwapi.SharedKernel/Exchange/DwhManifest.cs
+++ Dwapi.SharedKernel/Exchange/DwhManifest.cs
@@ -36,25 +36,25 @@ namespace Dwapi.SharedKernel.Exchange
 
         public static IEnumerable<DwhManifest> Create(IEnumerable<SitePatientProfile> profiles)
         {
-
+            /*
             var getPks = profiles.ToList();
             var list = new List<DwhManifest>();
 
             if (getPks.Any())
                 list.Add(new DwhManifest(getPks.First().SiteCode, getPks.Select(x => x.PatientPk).ToList()));
 
-            return list;
+            return list;*/
 
             // multi site setup
-            /*
-            var getPks = profiles.ToList().GroupBy(x => x.SiteCode).ToList();
+            
+            var groupPKsBySiteCode = profiles.ToList().GroupBy(x => x.SiteCode).ToList();
             var list = new List<DwhManifest>();
 
-            foreach (var pk in getPks)
+            foreach (var pk in groupPKsBySiteCode)
                 list.Add(new DwhManifest(pk.First().SiteCode, pk.Select(x => x.PatientPk).ToList()));
 
             return list;
-            */
+            
         }
 
         public void AddCargo(Metric metric)
