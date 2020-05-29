7d63db3cd384b80cf8ec6b85bd79c3ae75f665ea
Koske Kimutai <koskedk@live.com>
2020-05-29 14:32:30 +0300

    enhanced metric stage

---
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20200529112307_MetricStage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
+{
+    public partial class MetricStage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Stage",
+                table: "TempMetricMigrationExtracts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Stage",
+                table: "MetricMigrationExtracts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stage",
+                table: "TempMetricMigrationExtracts");
+
+            migrationBuilder.DropColumn(
+                name: "Stage",
+                table: "MetricMigrationExtracts");
+        }
+    }
+}
