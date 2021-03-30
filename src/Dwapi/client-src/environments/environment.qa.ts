14ca88e88c0d10d5329235bf8e25671a59303aa3
Koske Kimutai <koskedk@live.com>
2021-03-30 11:14:59 +0300

    v2509

--- src/Dwapi/client-src/environments/environment.qa.ts
+++ src/Dwapi/client-src/environments/environment.qa.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     port: '5753',
     emrs: [
-        {name: 'KenyaEMR', version: '17.3.2'},
+        {name: 'KenyaEMR', version: '17.3.3'},
         {name: 'IQCare', version: '2.2.1'}
     ]
 };
