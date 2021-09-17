ddffd7378e1b93afa39cf045422fc8c4626a6703
Koske Kimutai <koskedk@live.com>
2021-09-17 12:50:43 +0300

    covid extract final

--- src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20210917091456_CovidSeqView.Designer.cs
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20210917091456_CovidSeqView.Designer.cs
@@ -4,14 +4,16 @@ using Dwapi.ExtractsManagement.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ExtractsContext))]
-    partial class ExtractsContextModelSnapshot : ModelSnapshot
+    [Migration("20210917091456_CovidSeqView")]
+    partial class CovidSeqView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
