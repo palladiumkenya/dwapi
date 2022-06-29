effd3817eca21c0ac79b958fe1424e22c456cb55
marykilewe-palladium <mary.kilewe@thepalladiumgroup.com>
2022-06-29 23:05:48 +0300

    added NUPI to hts,patients,mnch and prep extracts

--- src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220629125019_HtsMnchDwhPrep_ExtractsNUPI.cs
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220629125019_HtsMnchDwhPrep_ExtractsNUPI.cs
@@ -7,81 +7,81 @@ namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "TempPatientPrepExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "TempPatientMnchExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "TempPatientExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "TempHtsClientExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "PatientPrepExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "PatientMnchExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "PatientExtracts",
-            //     nullable: true);
-            //
-            // migrationBuilder.AddColumn<string>(
-            //     name: "NUPI",
-            //     table: "HtsClientExtracts",
-            //     nullable: true);
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "TempPatientPrepExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "TempPatientMnchExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "TempPatientExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "TempHtsClientExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "PatientPrepExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "PatientMnchExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "PatientExtracts",
+                nullable: true);
+            
+            migrationBuilder.AddColumn<string>(
+                name: "NUPI",
+                table: "HtsClientExtracts",
+                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "TempPatientPrepExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "TempPatientMnchExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "TempPatientExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "TempHtsClientExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "PatientPrepExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "PatientMnchExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "PatientExtracts");
-            //
-            // migrationBuilder.DropColumn(
-            //     name: "NUPI",
-            //     table: "HtsClientExtracts");
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "TempPatientPrepExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "TempPatientMnchExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "TempPatientExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "TempHtsClientExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "PatientPrepExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "PatientMnchExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "PatientExtracts");
+            
+            migrationBuilder.DropColumn(
+                name: "NUPI",
+                table: "HtsClientExtracts");
         }
     }
 }
