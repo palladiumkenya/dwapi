af1c1151b649303451e698e110b003cf11c9fd34
Koske Kimutai <koskedk@live.com>
2022-07-14 10:13:42 +0300

    fix view

--- src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220629130047_HtsMnchDwhPrep_ExtractsNUPIViews.cs
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220629130047_HtsMnchDwhPrep_ExtractsNUPIViews.cs
@@ -154,44 +154,44 @@ namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
 						AS
 						SELECT        ValidationError.Id, Validator.Extract, Validator.Field, Validator.Type, Validator.Summary, ValidationError.DateGenerated, ValidationError.RecordId,
 
-					vTempPatientExtractError.FacilityName 
-				        vTempPatientExtractError.Gender 
-				        vTempPatientExtractError.DOB 
-				        vTempPatientExtractError.RegistrationDate 
-				        vTempPatientExtractError.RegistrationAtCCC 
-				        vTempPatientExtractError.RegistrationATPMTCT 
-				        vTempPatientExtractError.RegistrationAtTBClinic 
-				        vTempPatientExtractError.PatientSource 
-				        vTempPatientExtractError.Region 
-				        vTempPatientExtractError.District 
-				        vTempPatientExtractError.Village 
-				        vTempPatientExtractError.ContactRelation 
-				        vTempPatientExtractError.LastVisit 
-				        vTempPatientExtractError.MaritalStatus 
-				        vTempPatientExtractError.EducationLevel 
-				        vTempPatientExtractError.DateConfirmedHIVPositive 
-				        vTempPatientExtractError.PreviousARTExposure 
-				        vTempPatientExtractError.DatePreviousARTStart 
-				        vTempPatientExtractError.StatusAtCCC 
-				        vTempPatientExtractError.StatusAtPMTCT 
-				        vTempPatientExtractError.StatusAtTBClinic 
+				        vTempPatientExtractError.PatientID,
+					    vTempPatientExtractError.FacilityName, 
+				        vTempPatientExtractError.Gender, 
+				        vTempPatientExtractError.DOB, 
+				        vTempPatientExtractError.RegistrationDate, 
+				        vTempPatientExtractError.RegistrationAtCCC, 
+				        vTempPatientExtractError.RegistrationATPMTCT, 
+				        vTempPatientExtractError.RegistrationAtTBClinic, 
+				        vTempPatientExtractError.PatientSource, 
+				        vTempPatientExtractError.Region, 
+				        vTempPatientExtractError.District, 
+				        vTempPatientExtractError.Village, 
+				        vTempPatientExtractError.ContactRelation, 
+				        vTempPatientExtractError.LastVisit, 
+				        vTempPatientExtractError.MaritalStatus, 
+				        vTempPatientExtractError.EducationLevel, 
+				        vTempPatientExtractError.DateConfirmedHIVPositive, 
+				        vTempPatientExtractError.PreviousARTExposure,
+				        vTempPatientExtractError.StatusAtCCC,
+				        vTempPatientExtractError.StatusAtPMTCT,
+				        vTempPatientExtractError.StatusAtTBClinic, 
 
-				        vTempPatientExtractError.Orphan 
-				        vTempPatientExtractError.Inschool 
-				        vTempPatientExtractError.PatientType 
-				        vTempPatientExtractError.PopulationType 
-				        vTempPatientExtractError.KeyPopulationType 
-				        vTempPatientExtractError.PatientResidentCounty 
-				        vTempPatientExtractError.PatientResidentSubCounty 
-				        vTempPatientExtractError.PatientResidentLocation 
-				        vTempPatientExtractError.PatientResidentSubLocation 
-				        vTempPatientExtractError.PatientResidentWard 
-				        vTempPatientExtractError.PatientResidentVillage 
-				        vTempPatientExtractError.TransferInDate 
-				        vTempPatientExtractError.Date_Created 
-				        vTempPatientExtractError.Date_Last_Modified 
-				        vTempPatientExtractError.Pkv 
-				        vTempPatientExtractError.Occupation 
+				        vTempPatientExtractError.Orphan, 
+				        vTempPatientExtractError.Inschool, 
+				        vTempPatientExtractError.PatientType, 
+				        vTempPatientExtractError.PopulationType, 
+				        vTempPatientExtractError.KeyPopulationType, 
+				        vTempPatientExtractError.PatientResidentCounty, 
+				        vTempPatientExtractError.PatientResidentSubCounty, 
+				        vTempPatientExtractError.PatientResidentLocation, 
+				        vTempPatientExtractError.PatientResidentSubLocation, 
+				        vTempPatientExtractError.PatientResidentWard ,
+				        vTempPatientExtractError.PatientResidentVillage, 
+				        vTempPatientExtractError.TransferInDate, 
+				        vTempPatientExtractError.Date_Created,
+				        vTempPatientExtractError.Date_Last_Modified, 
+				        vTempPatientExtractError.Pkv,
+				        vTempPatientExtractError.Occupation,
 				        vTempPatientExtractError.NUPI 
 									 
 					FROM            vTempPatientExtractError INNER JOIN
