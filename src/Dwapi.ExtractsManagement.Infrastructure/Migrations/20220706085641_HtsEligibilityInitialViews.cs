95f3df5f6cf9d59a17ba08f5a57f2e0c9eb33b68
Koske Kimutai <koskedk@live.com>
2022-08-04 16:03:51 +0300

    colation update

--- src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220706085641_HtsEligibilityInitialViews.cs
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20220706085641_HtsEligibilityInitialViews.cs
@@ -7,6 +7,15 @@ namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
 	        
+	        if (migrationBuilder.ActiveProvider.ToLower().Contains("MySql".ToLower()))
+	        {
+		        migrationBuilder.Sql(@"alter table HtsEligibilityExtracts convert to character set utf8 collate utf8_unicode_ci;");
+		        migrationBuilder.Sql(@"alter table TempHtsEligibilityExtracts convert to character set utf8 collate utf8_unicode_ci;");
+		        migrationBuilder.Sql(@"alter table ClientRegistryExtracts convert to character set utf8 collate utf8_unicode_ci;");
+		        migrationBuilder.Sql(@"alter table TempClientRegistryExtracts convert to character set utf8 collate utf8_unicode_ci;");
+	        }
+
+	        
 			migrationBuilder.Sql(@"create view vTempHtsEligibilityExtractError as SELECT * FROM TempHtsEligibilityExtracts WHERE (CheckError = 1)");
 
 	        migrationBuilder.Sql(@"
