366e0fb4d5eaee18f3cf92dd09e4cf2df5d6607e
MaryKilewe <marykilewe@gmail.com>
2023-10-31 00:40:11 +0300

    prep fix added

---
+++ src/Dwapi.ExtractsManagement.Infrastructure/Migrations/20231030212813_PrepFixPatientPKDups.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dwapi.ExtractsManagement.Infrastructure.Migrations
+{
+    public partial class PrepFixPatientPKDups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder.ActiveProvider.ToLower().Contains("MySql".ToLower()))
+            {
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepAdverseEventExtracts DROP CONSTRAINT FK_PrepAdverseEventExtracts_PatientPrepExtracts_SiteCode_Patient;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepBehaviourRiskExtracts DROP CONSTRAINT FK_PrepBehaviourRiskExtracts_PatientPrepExtracts_SiteCode_Patien;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepCareTerminationExtracts DROP CONSTRAINT FK_PrepCareTerminationExtracts_PatientPrepExtracts_SiteCode_Pati;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepLabExtracts DROP CONSTRAINT FK_PrepLabExtracts_PatientPrepExtracts_SiteCode_PatientPK;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepPharmacyExtracts DROP CONSTRAINT FK_PrepPharmacyExtracts_PatientPrepExtracts_SiteCode_PatientPK;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepVisitExtracts DROP CONSTRAINT FK_PrepVisitExtracts_PatientPrepExtracts_SiteCode_PatientPK;");
+                migrationBuilder.Sql(@"ALTER TABLE dwapi.PrepMonthlyRefillExtracts DROP CONSTRAINT FK_PrepMonthlyRefillExtracts_PatientPrepExtracts_SiteCode_Patien;");
+               
+                migrationBuilder.Sql(@"ALTER TABLE `dwapi`.`PatientPrepExtracts` 
+                        DROP PRIMARY KEY,
+                        ADD PRIMARY KEY (`SiteCode`, `Id`);
+                        ");
+            }
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
